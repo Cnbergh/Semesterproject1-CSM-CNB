439763afd8db998d63d0d2ae94c8fd6a693422b0
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2022-12-18 21:54:39 +0100

    sdfsdf

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@ function toogleHamburger() {
     const nav = document.getElementById('sidebar-menu');
     nav.classList.toggle('expanded')
 }
+function submitcontactform() {
+    alert("The form was submitted");
+  }
+  function openCheckout() {
+    const checkout=document.getElementById('checkout');
+    checkout.classList.add('expanded')
+  }
